5745a410589d4e334ecfc3172ea39b586ec9c46c
Yan Lau <joyce.lau@uqconnect.edu.au>
2017-10-13 15:25:23 +1100

    sdkfnds

--- index.js
+++ index.js
@@ -131,6 +131,7 @@ function sendMessage(recipientId, message) {
 function processMessage(event) {
 	console.log("here");
 	console.log(event.message);
+	event.message.is_echo = false;
   if (!event.message.is_echo) {
     var message = event.message;
     var senderId = event.sender.id;
