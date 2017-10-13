648e3e8127e3b5de6efb900be05575ac84fe6ae2
Yan Lau <joyce.lau@uqconnect.edu.au>
2017-10-13 13:12:43 +1100

    access

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 /*
 Please report any bugs to nicomwaks@gmail.com
 
-i have added console.log on line 48 
+i have added console.log on line 48
 
 
 
@@ -45,7 +45,7 @@ app.post('/webhook/', function (req, res) {
 		let sender = event.sender.id
 		if (event.message && event.message.text) {
 			let text = event.message.text
-			if (text === 'Generic'){ 
+			if (text === 'Generic'){
 				console.log("welcome to chatbot")
 				//sendGenericMessage(sender)
 				continue
@@ -64,11 +64,11 @@ app.post('/webhook/', function (req, res) {
 
 // recommended to inject access tokens as environmental variables, e.g.
 // const token = process.env.FB_PAGE_ACCESS_TOKEN
-const token = "<FB_PAGE_ACCESS_TOKEN>"
+const token = "EAACU0onlW0oBAEqn1FPUgraLUD1v7n1ZCZCaMfaAz8gAHELMWT1DUfkwJkv9JHZBrictBnATxWr3B9pWLjDyyWGt135aQb9iFOHrX1maZCuoRURqkVVGf98z65iTRSVbxBwSNZCW6a9tVmV9VvO4bzZC8r0K9IJ85NeoXEVteHegZDZD"
 
 function sendTextMessage(sender, text) {
 	let messageData = { text:text }
-	
+
 	request({
 		url: 'https://graph.facebook.com/v2.6/me/messages',
 		qs: {access_token:token},
